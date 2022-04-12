97366cc9c60be40cbfa7fc2c21ba024011e35043
Dustin Eidemiller <eidemillerdustin@gmail.com>
2022-04-12 15:57:07 -0400

    completed add form

--- vue/src/components/PotholeMap.vue
+++ vue/src/components/PotholeMap.vue
@@ -4,7 +4,53 @@
       <div class="logo">
         <img src="../assets/images/pothole_tracker_logo.png" alt="Logo" />
       </div>
-
+      <div>
+        <form class="pothole-form" v-on:submit.prevent="submitForm">
+          <!-- v-show keeps it in the DOM, but adds display: none. This is ALWAYS a good idea for forms -->
+          <div class="form-element">
+            <input
+              id="crossStreet1"
+              type="text"
+              v-model="newPothole.crossStreet1"
+            />
+            <label for="crossStreet1">Street:</label>
+            <input
+              id="crossStreet2"
+              type="text"
+              v-model="newPothole.crossStreet2"
+            />
+            <label for="crossStreet2">Nearest Cross Street:</label>
+          </div>
+          
+          <div class="form-element">
+            <input id="date" type="date" v-model="newPothole.dateReported" />
+            <label for="date">Date Reported:</label>
+          </div>
+          <div class="form-element">
+            <input
+              type="text"
+              id="contactName"
+              v-model="newPothole.contactName"
+            />
+            <label for="contactName">Contact Name:</label>
+            <input
+              id="contactEmail"
+              type="email"
+              v-model="newPothole.contactEmail"
+            />
+            <label for="contactEmail">Contact Email:</label>
+            <input
+              type="text"
+              id="contactPhone"
+              v-model="newPothole.contactPhone"
+            />
+            <label for="contactPhone">Contact Phone:</label>
+          </div>
+          <input type="submit" value="Save" />
+          <!-- v-on:click.prevent="handleSave" -->
+          <input type="button" value="Cancel" />
+        </form>
+      </div>
       <div
         class="pothole-list"
         v-for="pothole in potholes"
@@ -85,7 +131,18 @@ export default {
       showForm: false,
       lat: "",
       lng: "",
+      newPothole: {
+        dateReported: "", // v-model="newReview.title"
+        contactName: "",
+        contactEmail: "",
+        contactPhone: "",
+        crossStreet1: "",
+        crossStreet2: "",
+        latitude: "",
+        longitude: "",
+      },
     };
+    
   },
   methods: {
     getPotholes() {
@@ -99,6 +156,15 @@ export default {
       this.showForm = true;
       this.lat = e.latlng.lat;
       this.lng = e.latlng.lng;
+      this.newPothole.latitude = e.latlng.lat;
+      this.newPothole.longitude = e.latlng.lng;
+    },
+    submitForm() {
+      PotholeService.add(this.newPothole).then((response) => {
+        if (response.status === 201 || response.status === 200) {
+          console.log('success');
+        }
+      });
     },
   },
   created() {
@@ -108,6 +174,35 @@ export default {
 </script>
 
 <style scoped>
+.pothole-form {
+  border-radius: 5px;
+  padding: 1.5em 2.25em;
+  margin-bottom: 1.75em;
+  font-size: 0.5em;
+  letter-spacing: 0.125em;
+  background-color: #42484d51;
+  color: #030303;
+}
+div.form-element {
+  margin-top: 10px;
+}
+div.form-element > label {
+  font-size: 10pt;
+  display: block;
+}
+div.form-element > input,
+div.form-element > select {
+  height: 15px;
+  width: 300px;
+}
+form > input[type="button"] {
+  width: 100px;
+}
+form > input[type="submit"] {
+  width: 100px;
+  margin-right: 10px;
+}
+
 #container {
   display: flex;
 }
