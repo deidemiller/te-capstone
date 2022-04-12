964ccf9f720730023b3902a24b70473598c3596e
ziwei zhang <ziwei632@gmail.com>
2022-04-12 10:43:07 -0400

    pushing

--- vue/src/components/PotholeMap.vue
+++ vue/src/components/PotholeMap.vue
@@ -4,6 +4,22 @@
       <div class="logo">
         <img src="../assets/images/pothole_tracker_logo.png" alt="Logo" />
       </div>
+      <div
+        class="pothole-list"
+        v-for="pothole in potholes"
+        v-bind:key="pothole.potholeId"
+      >
+        <h1>🔍{{ pothole.dateReported }}</h1>
+      </div>
+      <div class="option-2">
+        <div
+          class="pothole-list2"
+          v-for="pothole in potholes"
+          v-bind:key="pothole.potholeId"
+        >
+          <h1>🔍{{ pothole.dateReported }}</h1>
+        </div>
+      </div>
     </div>
     <div id="map">
       <l-map style="width: 100%" :zoom="zoom" :center="center">
@@ -16,7 +32,10 @@
             icon-url="https://i.postimg.cc/rmwdTRPL/imageedit-1-9960213275.png"
             :icon-size="[40, 40]"
           ></lIcon
-          ><l-popup content="This is a popup" />
+          ><l-popup class="popup"
+            >Reported on:<br />
+            {{ pothole.dateReported }}</l-popup
+          >
         </l-marker>
       </l-map>
     </div>
@@ -72,7 +91,10 @@ export default {
 .sideBar {
   background-color: #cddfe3;
   height: 100vh;
-  width: 30%;
+  width: 25%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  padding: 0.5em 1em 0.5em 1em;
 }
 .logo {
   margin: 0 auto;
@@ -85,5 +107,26 @@ img {
 #map {
   height: 100vh;
   flex: 1;
+}
+.pothole-list {
+  border-radius: 5px;
+  padding: 1.5em 2.25em;
+  margin-bottom: 1.75em;
+  font-size: 0.5em;
+  letter-spacing: 0.125em;
+  background-color: #42484d51;
+}
+.option-2 {
+  border-radius: 5px;
+  padding: 1.5em 2.25em;
+  margin-bottom: 1.75em;
+  font-size: 0.5em;
+  letter-spacing: 0.125em;
+  background-color: #42484d51;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 100px;
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
