4e9137adf6240b40985cde5a4694291dab8e2cfa
John Bargiel <john.bargiel.3@gmail.com>
2022-04-14 12:47:35 -0400

    Updated pothole Services

--- vue/src/services/PotholeService.js
+++ vue/src/services/PotholeService.js
@@ -9,5 +9,33 @@ export default {
   },
   add(pothole) {
     return http.post('/potholes/add', pothole);
+  },
+  listPending() {
+    return http.get('/potholes/pending');
+  },
+  listAll() {
+    return http.get('/potholes/all');
+  },
+  listRepaired() {
+    return http.get('/potholes/repaired');
+  },
+  listScheduled() {
+    return http.get('/potholes/scheduled');
+  },
+  listUnscheduled() {
+    return http.get('/potholes/unscheduled');
+  },
+  delete(pothole) {
+    http.delete('potholes/delete', pothole);
+  },
+  updatePending(pothole) {
+    http.put('/potholes/status/update', pothole);
+  },
+  updateRepairStatus(pothole) {
+    http.put('/potholes/repair_status/update', pothole);
+  },
+  repairedPothole(pothole) {
+    http.put('/potholes/complete', pothole)
   }
+
 };
