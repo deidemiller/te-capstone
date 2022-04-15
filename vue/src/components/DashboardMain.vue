038a3d362247f7e6007ecb1d31ed45bc21247831
John Bargiel <john.bargiel.3@gmail.com>
2022-04-14 21:13:25 -0400

    debugging verify

--- vue/src/components/DashboardMain.vue
+++ vue/src/components/DashboardMain.vue
@@ -57,7 +57,13 @@
                   {{ pothole.crossStreet2 }}
                 </td>
                 <td>
-                  <button class="button-35" role="button" v-on:click="verifyPothole(pothole.potholeId) ">Verify</button>
+                  <button
+                    class="button-35"
+                    role="button"
+                    v-on:click="verifyPothole(pothole.potholeId)"
+                  >
+                    Verify
+                  </button>
                 </td>
                 <td>
                   <button
@@ -150,25 +156,26 @@ export default {
       });
     },
     verifyPothole(id) {
-      for (let i=0; i<this.pendingPotholes.length; i++){
-        if (id===this.pendingPotholes[i]){
-          this.pothole=this.pendingPotholes[i];
+      for (let i = 0; i < this.pendingPotholes.length; i++) {
+        if (id === this.pendingPotholes[i]) {
+          this.pothole = this.pendingPotholes[i];
           break;
         }
       }
-      this.pothole.repairStatus="unscheduled";
-      PotholeService.updateRepairStatus(this.pothole).then(response => {
-        if (response.status===200){
-          PotholeService.updatePending(this.pothole).then(response => {
-            if (response.status===200){
-              console.log("cool")
+      this.pothole.repairStatus = "unscheduled";
+      PotholeService.updateRepairStatus(this.pothole).then((response) => {
+        if (response.status === 200) {
+          PotholeService.updatePending(this.pothole).then((response) => {
+            if (response.status === 200) {
+              this.pothole = {};
+              console.log("cool");
             }
           });
-          this.pothole={};
+
           this.getPendingPotholes();
         }
       });
-    }
+    },
   },
   created() {
     this.getPendingPotholes();
