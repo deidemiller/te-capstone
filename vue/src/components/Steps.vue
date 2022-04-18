e25bd5dd6d79a889e09e372356095805d1183654
ziwei zhang <ziwei632@gmail.com>
2022-04-18 14:17:30 -0400

    main page button

--- vue/src/components/Steps.vue
+++ vue/src/components/Steps.vue
@@ -8,6 +8,12 @@
         tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus
         eget. Nunc nibh morbi quis fusce lacus.
       </p>
+      <router-link
+        tag="button"
+        v-bind:to="{ name: 'potholes' }"
+        class="head_button"
+        ><button class="button-59" role="button">View</button></router-link
+      >
     </div>
 
     <div class="content content-left">
@@ -17,6 +23,9 @@
         tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus
         eget. Nunc nibh morbi quis fusce lacus.
       </p>
+      <router-link tag="button" to="/dashboard" class="head_button"
+        ><button class="button-59" role="button">View</button></router-link
+      >
     </div>
     <div class="img-right"><img src="../assets/images/fix.jpg" alt="" /></div>
   </div>
@@ -42,6 +51,7 @@ export default {
 img {
   width: 100%;
 }
+
 .content {
   padding: 3.5em;
   vertical-align: middle;
@@ -69,5 +79,58 @@ img {
 .content-left {
   grid-area: content-left;
   text-align: right;
+}
+.button-59 {
+  align-items: center;
+  background-color: #fff;
+  border: 2px solid #000;
+  box-sizing: border-box;
+  color: #000;
+  cursor: pointer;
+  display: inline-flex;
+  fill: #000;
+  font-family: Inter, sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  height: 48px;
+  justify-content: center;
+  letter-spacing: -0.8px;
+  line-height: 24px;
+  min-width: 140px;
+  outline: 0;
+  padding: 0 17px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 0.3s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-59:focus {
+  color: #171e29;
+}
+
+.button-59:hover {
+  border-color: #06f;
+  color: white;
+  fill: white;
+  background-color: #06f;
+}
+
+.button-59:active {
+  border-color: #06f;
+  color: white;
+  fill: white;
+  background-color: #06f;
+}
+
+@media (min-width: 768px) {
+  .button-59 {
+    min-width: 170px;
+  }
+}
+.head_button {
+  border: none;
 }
 </style>
